72c8e784af83758e57cc2d271241a3d207aa1e3a
Russel <russel@overdrive.co.za>
2023-01-22 19:01:42 +0200

    Add project files.

---
+++ GiraffeTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GiraffeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ini
+            string name;
+            string age;
+
+            Console.Write("Enter your name: "); //Text to ask user their name
+            name = Console.ReadLine(); //Allow user to enter their name
+
+            Console.WriteLine("");
+
+            Console.Write("Enter your age: "); //Text to ask user their age
+            age = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
